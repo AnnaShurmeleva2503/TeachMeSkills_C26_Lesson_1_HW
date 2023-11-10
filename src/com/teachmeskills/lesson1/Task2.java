51bb8fb302b5ec06826bc9ca103bf1d351e73e68
AnnaShurmeleva25 <anna.shurmeleva@mail.ru>
2023-11-10 23:46:06 +0300

    My homework for first lesson

---
+++ src/com/teachmeskills/lesson1/Task2.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.lesson1;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String user = scanner.nextLine();
+        System.out.println("Hello " + user);
+
+    }
+}
